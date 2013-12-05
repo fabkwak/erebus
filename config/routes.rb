2b4b685d19a012ead4c13f182f4eb7ee4b2326f8
fabkwak <fabkwak@yahoo.com>
2013-12-04 20:46:54 -0800

    navbar and static

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 ErebusApp::Application.routes.draw do
   root :to => "static_pages#home"
  
-  #get "static_pages/home"
-  get "static_pages/about"
-  get "static_pages/contact"
-
+  match '/contact', to: 'static_pages#contact'
+  match '/about',   to: 'static_pages#about'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
