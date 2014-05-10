7c0254540c61eb14652e7f5f5a58b5295189ef1b
Fabian <fabkwak@yahoo.com>
2014-05-09 17:08:36 -0700

    controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 ErebusApp::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
   root :to => "static_pages#home"
- 
-  match '/signup',  to: 'users#new'
+  match '/signup',  to: 'users#new',             via: 'get'
+  match '/signin',  to: 'sessions#new',          via: 'get'
+  match '/signout', to: 'sessions#destroy',      via: 'delete'
   match '/contact', to: 'static_pages#contact'
   match '/about',   to: 'static_pages#about'
   
